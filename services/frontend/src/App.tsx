f21bbc54a92923e91e367df886ce3f1fb0101491
ggichuru <ggichuru.dev@gmail.com>
2023-10-14 20:54:25 +0300

    setup wallet

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MyWallet from "./components/WalletComponent/Wallet";
+import SetBudget from "./components/WalletComponent/SetBudget";
+import MyWalletAvailableBalance from "./components/WalletComponent/MyWalletAvailableBalance";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/wallet" element={<MyWallet />} />
+        <Route path="*" element={<MyWallet />} />
+        <Route path="/balance" element={<MyWalletAvailableBalance />} />
+        <Route path="/budget" element={<SetBudget />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ services/frontend/src/components/WalletComponent/index.ts
@@ -0,0 +1,3 @@
+export * from "./MyWalletAvailableBalance";
+export * from "./SetBudget";
+export * from "./Wallet";
